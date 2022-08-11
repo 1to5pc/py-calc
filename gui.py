c1c88d7dfea27c441ffdaf6837483f2d88927e74
ravinu <ravinu.gcom@gmail.com>
2022-08-11 11:45:30 +0530

    comment unwanted lines

--- gui.py
+++ gui.py
@@ -149,31 +149,31 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-while True:
-    choice = input("Enter choice(1/2/3/4): ")
-    if choice in ('1', '2', '3', '4'):
-        num1 = int(input("Enter first number: "))
-        num2 = int(input("Enter second number: "))
+#while True:
+    #choice = input("Enter choice(1/2/3/4): ")
+    #if choice in ('1', '2', '3', '4'):
+    #    num1 = int(input("Enter first number: "))
+    #    num2 = int(input("Enter second number: "))
 
-        if choice == '1':
-            print(num1, "+", num2, "=", add(num1, num2))
+   #     if choice == '1':
+  #          print(num1, "+", num2, "=", add(num1, num2))
 
-        elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+ #       elif choice == '2':
+#            print(num1, "-", num2, "=", subtract(num1, num2))
 
-        elif choice == '3':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+ #       elif choice == '3':
+#            print(num1, "*", num2, "=", multiply(num1, num2))
 
-        elif choice == '4':
-            print(num1, "/", num2, "=", divide(num1, num2))
+ #       elif choice == '4':
+ #           print(num1, "/", num2, "=", divide(num1, num2))
 
-        next_calculation = input("Let's do next calculation? (Y/n): ")
-        if next_calculation == "n":
-          break
+#        next_calculation = input("Let's do next calculation? (Y/n): ")
+#        if next_calculation == "n":
+#          break
         #elif next_calculation == "":
           #break
         #else:
             #break
-
-    else:
-        print("Invalid Input")
+#
+#    else:
+#        print("Invalid Input")
